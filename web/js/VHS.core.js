e629ad229a4175653a52641e4f48c56d30098c8a
Nico Panfili <nico@beyondrarity.com>
2026-04-16 11:58:41 -0300

    Add some logging

--- web/js/VHS.core.js
+++ web/js/VHS.core.js
@@ -812,8 +812,11 @@ function addUploadWidget(nodeType, nodeData, widgetName, type="video") {
                 let resp = await uploadFile(file, (p) => node.progress = p)
                 node.progress = undefined
                 if (resp.status != 200) {
+                    console.log('[VHS] - Failed to upload file')
                     return false
                 }
+                console.log('[VHS] - File uploaded successfully')
+                console.log('[VHS] - Response:', JSON.parse(resp.responseText))
                 const filename = JSON.parse(resp.responseText).name;
                 pathWidget.options.values.push(filename);
                 pathWidget.value = filename;
